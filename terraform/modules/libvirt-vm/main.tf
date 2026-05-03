1b9fba8d6bcc7150fdccf2d3ad89ab3594ab5b01
Dmitry <>
2026-05-03 16:10:15 +0300

    changed meta-data and set hostname to fqdn

--- terraform/modules/libvirt-vm/main.tf
+++ terraform/modules/libvirt-vm/main.tf
@@ -14,7 +14,6 @@ resource "libvirt_cloudinit_disk" "cloudinit" {
   name = "${var.vm_name}-cloudinit"
   meta_data = yamlencode({
     instance-id    = "${var.vm_name}-cloudinit"
-    local-hostname = "${var.vm_name}"
   })
   user_data      = <<-EOF
     #cloud-config
@@ -22,6 +21,7 @@ resource "libvirt_cloudinit_disk" "cloudinit" {
     prefer_fqdn_over_hostname: true
     create_hostname_file: true
     manage_etc_hosts: true
+    preserve_hostname: false
     bootcmd:
       - echo nameserver 8.8.8.8 > /etc/resolv.conf
     runcmd:
