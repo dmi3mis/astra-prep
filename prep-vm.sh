fe519159960c69a2a8f2db7c671c66e4baa4d803
Dmitry <11094711+dmi3mis@users.noreply.github.com>
2026-05-04 15:04:16 +0300

    Refactor prep-vm.sh for cleaner installation process
    Simplified the command to run the aldpro-client-installer and removed unnecessary commands.

--- prep-vm.sh
+++ prep-vm.sh
@@ -6,7 +6,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -y -q aldpro-client
 ip4=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
 
 sudo sed -i "s/127.0.1.1/${ip4}/g" /etc/hosts
-rm /etc/resolv.conf
+sudo rm /etc/resolv.conf
 
 sudo bash -c 'cat << EOF > /etc/resolv.conf 2>/dev/null
 nameserver 192.168.101.201
@@ -16,10 +16,7 @@ search ald.test
 EOF'
 
 set +o history
-sudo /opt/rbta/aldpro/client/bin/aldpro-client-installer \
-    --domain ald.test \
-    --account admin \
-    --password 'password' \ 
-    --host $(hostname) \
-    --gui --force && sudo reboot
+sudo /opt/rbta/aldpro/client/bin/aldpro-client-installer --domain ald.test --account admin --password password --host $(hostname --short)  --gui --force
 set -o history
+
+sudo reboot
